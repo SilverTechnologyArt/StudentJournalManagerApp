7531c8ea0e1e8ff6e5ed46d51ae6ccf028dae98e
Mariusz Zajk <zajk.mariusz@wp.pl>
2023-07-14 10:08:28 +0200

    initial comit

--- Student Journal Manager/Models/LessonAttendance.cs
+++ Student Journal Manager/Models/LessonAttendance.cs
@@ -1,6 +1,14 @@
-﻿namespace Student_Journal_Manager.Models
+﻿using System;
+
+namespace Student_Journal_Manager.Models
 {
     public class LessonAttendance
     {
+        public int Id { get; set; }
+        public int LessonId { get; set; }
+        public int StudentId { get; set; }
+        public DateTime Date { get; set; }
+        public int Hours { get; set; }
+        public bool IsLocked { get; set; }
     }
 }
